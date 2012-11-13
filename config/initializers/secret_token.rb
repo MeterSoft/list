e523a92d5f2be840ef1eb6e37d65664520585a9c
Meter <meter@meter-System-Product-Name.(none)>
2012-11-13 17:35:29 +0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+List::Application.config.secret_token = '0d0938f1de833c2ace97264fbc1c4dee30c1fe1a8fc50d1d1248abc23af19c504a1c44f42a29c478459fafebb29f714fa2f5ee208b198ffefb58357644e391be'
