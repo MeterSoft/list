237145ce4ab3431ec8e6ae900f1f7af15bcb6527
Meter <meter@meter-System-Product-Name.(none)>
2012-11-30 16:05:46 +0200

    seting desice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  #before_filter :determine_layout
+  #layout :determine_layout
   before_filter :authenticate_user!
+
+  layout :layout_by_resource
 
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    categories_path
+  end
+
+  def layout_by_resource
+    if devise_controller?
+      "start_page"
+    else
+      "application"
+    end
   end
 
 end
