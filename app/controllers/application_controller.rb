cd1c6ee1d5ace13304b09e7766ee72dcba73476f
Meter <meter@meter-System-Product-Name.(none)>
2012-12-04 17:23:21 +0200

    kolypau

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,24 +2,24 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   #before_filter :determine_layout
   #layout :determine_layout
-  before_filter :authenticate_user!
+  #before_filter :authenticate_user!
 
   layout :layout_by_resource
-
-  protected
-
-  def after_sign_out_path_for(resource_or_scope)
-    new_user_session_path
-  end
-
-  def after_sign_in_path_for(resource_or_scope)
-    categories_path
-  end
-
-  def after_inactive_sign_up_path_for(resource_or_scope)
-     categories_path
-  end
-
+#
+#  protected
+#
+#  def after_sign_out_path_for(resource_or_scope)
+#    new_user_session_path
+#  end
+#
+#  def after_sign_in_path_for(resource_or_scope)
+#    categories_path
+#  end
+#
+#  def after_inactive_sign_up_path_for(resource_or_scope)
+#     categories_path
+#  end
+#
   def layout_by_resource
     if devise_controller?
       "start_page"
